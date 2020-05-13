0a03148c8175d78b520d88c93e1ac27fd6614b29
Melissa <Melissalammm@gmail.com>
2020-05-13 17:34:52 -0500

    edit

--- src/Components/Functional.js
+++ src/Components/Functional.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 
 function Functional(){
         return (
+            <footer>
             <div className = 'contact'>
                 <p>Instagram: @DOODLETAILSADOPTION </p>
                 <p>Email: DOODLETAILS@GMAIL.COM</p>
                 <p>PHONE: 123-456-7890</p>
             </div>
+            </footer>
         )
     }
 
